50562acf301adc7ed83d17c3c5626bb1faeb4ff7
7Romain <osiiriis@ymail.com>
2022-11-02 08:59:49 +0100

    numtel

---
+++ src/main/resources/static/js/numtel.js
@@ -0,0 +1,22 @@
+// function Autotab(box, longueur, texte) {
+//     if (texte.length > longueur - 1) {
+//         document.getElementsByName("tel[]")[box].focus();
+//     }
+// }
+console.log("ici");
+function formatTel(num) {
+    return num.replace(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, "$1 $2 $3 $4 $5");
+}
+const formulairePersonne = document.getElementById("formulairePersonne");
+const inputTel = document.getElementById("telephoneInput");
+inputTel.addEventListener("blur", function (e) {
+    console.log(inputTel.value);
+    inputTel.value = formatTel(inputTel.value);
+    console.log(inputTel.value);
+});
+
+formulairePersonne.addEventListener("submit", function (e) {
+    console.log(inputTel.value);
+    inputTel.value = formatTel(inputTel.value);
+    console.log(inputTel.value);
+});
